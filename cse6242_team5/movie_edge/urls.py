09773620b145ffbf924132279cd46116539e146c
Daniel Klass <git@danielklass.com>
2019-10-20 15:18:48 -0700

    Wiring up new views and ensuring they are valid.

--- cse6242_team5/movie_edge/urls.py
+++ cse6242_team5/movie_edge/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('query_recommendations/<str:gensim_model_str>/', views.query_recommendations),
 ]
--- cse6242_team5/movie_edge/views.py
+++ cse6242_team5/movie_edge/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.http import JsonResponse
+from pathlib import Path
+
+gensim_models = dict()
+current_path = Path('.')
+
+# You will probably need to update this
+gensim_path = current_path / '..' / 'gensim_models2'
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    return HttpResponse('You may be looking for query_recommendations/<str:gensim_model>/')
+
+
+def query_recommendations(request: HttpRequest,
+                          gensim_model_str: str):
+    # Making sure model data is fine
+    assert gensim_path.is_dir(), "Gensim Directory Not Correct"
+
+    gensim_model_path = gensim_path / gensim_model_str
+    if not gensim_model_path.exists():
+        raise FileNotFoundError
+
+
+    return HttpResponse('I have some recommendations for you!')
