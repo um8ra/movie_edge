f66eafa8b5298a0911275f910d220f3af0ec712b
Daniel Klass <git@danielklass.com>
2019-10-20 19:55:25 -0700

    Adding movie database class so that depending on model, we can retrieve and send this data to the client quickly.

--- cse6242_team5/movie_edge/models.py
+++ cse6242_team5/movie_edge/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    embedder = models.CharField(max_length=128, db_index=True, null=False)  # The model that ran this data
+    movie_id = models.IntegerField(null=False)  # Movie ID based on movielens data
+    movie_title = models.CharField(max_length=256, null=False)
+    genres = models.CharField(max_length=128, null=False)
+    x = models.FloatField(null=False)  # X value post t-SNE
+    y = models.FloatField(null=False)  # Y value post t-SNE
+    cluster = models.IntegerField(null=False)  # Agglomerative clustering pre-t-SNE
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['embedder', 'movie_id'], name='unique_movie_per_embedding'),
+        ]
