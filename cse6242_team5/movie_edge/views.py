3c79ecc6c27ec7a72aafdbfc1787f18a279c3fba
Daniel Klass <git@danielklass.com>
2019-10-20 18:43:08 -0700

    Form submission works. Want to echo out actual movie data.

--- cse6242_team5/movie_edge/views.py
+++ cse6242_team5/movie_edge/views.py
@@ -11,22 +11,21 @@ print(BASE_DIR)
 
 dict_gensim_models = dict()
 base_path = Path(BASE_DIR)
-print(base_path)
 
 # You will probably need to update this
 gensim_path = base_path / '..' / 'gensim_models2'
 
+
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('You may be looking for query_recommendations/<str:gensim_model>/')
 
-def sentiment_form(request: HttpRequest) -> HttpResponse:
-    # template = loader.get_template('movie_edge/sentiment_form.html')
 
+def sentiment_form(request: HttpRequest) -> HttpResponse:
     return render(request, 'movie_edge/sentiment_form.html',
                   {'form': SentimentForm()})
 
 
-def query_recommendations(request: HttpRequest) -> HttpResponse:
+def query_recommendations(request: HttpRequest, topn=5) -> HttpResponse:
     # Making sure model data is fine
     assert gensim_path.is_dir(), "Gensim Directory Not Correct"
 
@@ -36,20 +35,24 @@ def query_recommendations(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             print(form.cleaned_data)
             gensim_model_str = form.cleaned_data['gensim_model']
-            movies_liked = form.cleaned_data['likes']
-            movies_disliked = form.cleaned_data['dislikes']
+            movies_liked = form.cleaned_data['likes'].split(',')
+            movies_disliked = form.cleaned_data['dislikes'].split(',')
+            print(movies_liked)
+            print(movies_disliked)
 
             gensim_model_path = gensim_path / gensim_model_str
             if not gensim_model_path.is_file():
                 raise FileNotFoundError
 
-            print(gensim_model_path)
             if gensim_model_str in dict_gensim_models.keys():
                 model = dict_gensim_models[gensim_model_str]
             else:
                 model = Word2Vec.load(str(gensim_model_path))
                 dict_gensim_models[gensim_model_str] = model
 
+            movies_similar = model.most_similar(positive=movies_liked,
+                                                negative=movies_disliked,
+                                                topn=topn)
 
 
     return HttpResponse('I have some recommendations for you!')
