0dc8adc86c07f8f1735c68ca988d56241105af44
Daniel Klass <git@danielklass.com>
2019-11-05 19:08:03 -0800

    Starting with nine random frequently rated movies.

--- cse6242_team5/movie_edge/views.py
+++ cse6242_team5/movie_edge/views.py
@@ -10,6 +10,7 @@ import pandas as pd
 import json
 import urllib.parse
 from bokeh import palettes
+import random
 
 MOVIE_ID = 'movie_id'
 MOVIE_TITLE = 'movie_title'
@@ -59,10 +60,17 @@ def index(request: HttpRequest) -> HttpResponse:
     movies_y_min = movies.aggregate(Min(Y))
     movies_y_max = movies.aggregate(Max(Y))
 
+    frequently_rated_movies = movies.filter(imdb_votes__gte=10000)
+    len_movies = len(frequently_rated_movies)
+    randoms = [random.randint(0, len_movies - 1) for _ in range(9)]
+    serendipity_movies = [frequently_rated_movies[i] for i in randoms]
+
+    print(serendipity_movies)
     data = {
         'data': list(movies),
         # Since D3 likes to operate on arrays, this decodes movie-id to array position
         'decoder': {m[MOVIE_ID]: i for i, m in enumerate(movies)},
+        'grid': serendipity_movies,
         **movies_x_min,
         **movies_x_max,
         **movies_y_min,
