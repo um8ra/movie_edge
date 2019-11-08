1bf623f255a4a50e1e7b2cf460beb8526b13fb09
unknown <jtay6@gatech.edu>
2019-11-09 00:05:59 +1100

    init

--- cse6242_team5/movie_edge/views.py
+++ cse6242_team5/movie_edge/views.py
@@ -11,6 +11,7 @@ import json
 import urllib.parse
 from bokeh import palettes
 import random
+from typing import List
 
 MOVIE_ID = 'movie_id'
 MOVIE_TITLE = 'movie_title'
@@ -31,9 +32,16 @@ METASCORE = 'metascore'
 IMDB_RATING = 'imdb_rating'
 IMDB_VOTES = 'imdb_votes'
 
+MOVIE_CHOICES = 'movie_choices'
+LIKE = 'movies_liked'
+DISLIKE = 'movies_disliked'
+
+
 EMBEDDER = 'w2v_vs_64_sg_1_hs_1_mc_1_it_4_wn_32_ng_2_all_data_trg_val_tst.gensim'
 
-db_cols = [MOVIE_ID, MOVIE_TITLE, TITLE, GENRES, X, Y, MEAN, COUNT, STDDEV, CLUSTER, POSTER_URL, RUNTIME, DIRECTOR,
+
+
+db_cols = [MOVIE_ID, MOVIE_TITLE, TITLE, GENRES, MEAN, COUNT, STDDEV]+  [f'L{i}x' for i in range(6)]+[f'L{i}y' for i in range(6)] +  [POSTER_URL, RUNTIME, DIRECTOR,
            ACTORS, METASCORE, IMDB_RATING, IMDB_VOTES]
 
 dict_gensim_models = dict()
@@ -47,19 +55,29 @@ df_movies = pd.read_csv(str(movie_df_path), index_col='movieId', dtype=str)
 df_movies.index.rename(MOVIE_ID, inplace=True)
 
 
+def random_movie_ids(n: int, imdb_votes=10000) -> List[int]:
+    # https://stackoverflow.com/questions/1731346/how-to-get-two-random-records-with-django
+    all_movie_ids = Movie.objects.filter(embedder=EMBEDDER, imdb_votes__gte=imdb_votes).values_list('id', flat=True)
+    random_movies = random.sample(list(all_movie_ids), n)
+    return_val = Movie.objects.filter(id__in=random_movies).values_list(MOVIE_ID, flat=True)
+    print('Random Movies!')
+    print(return_val)
+    return list(return_val)
+
+
 def index(request: HttpRequest) -> HttpResponse:
     movies = Movie.objects.filter(embedder=EMBEDDER).values(*db_cols)
-    palette = palettes.Category20_20
+    #palette = palettes.Category20_20
     for movie in movies:
         # This is done since quotes and other junk in the title screws up JSON parsing
         movie[MOVIE_TITLE] = urllib.parse.quote(movie[MOVIE_TITLE])
         movie[DIRECTOR] = urllib.parse.quote(movie[DIRECTOR])
         movie[ACTORS] = urllib.parse.quote(movie[ACTORS])
-        movie[COLOR] = palette[movie[CLUSTER]]
-    movies_x_min = movies.aggregate(Min(X))
-    movies_x_max = movies.aggregate(Max(X))
-    movies_y_min = movies.aggregate(Min(Y))
-    movies_y_max = movies.aggregate(Max(Y))
+       
+    movies_x_min = movies.aggregate(Min('L5x'))
+    movies_x_max = movies.aggregate(Max('L5x'))
+    movies_y_min = movies.aggregate(Min('L5y'))
+    movies_y_max = movies.aggregate(Max('L5y'))
 
     frequently_rated_movies = movies.filter(imdb_votes__gte=10000)
     len_movies = len(frequently_rated_movies)
@@ -79,8 +97,7 @@ def index(request: HttpRequest) -> HttpResponse:
 
     data_json = json.dumps(data)
     return render(request, 'movie_edge/visualization.html',
-                  {'table_data': data_json,
-                   'form': SentimentForm})
+                  {'table_data': data_json})
 
 
 def sentiment_form(request: HttpRequest) -> HttpResponse:
@@ -88,42 +105,47 @@ def sentiment_form(request: HttpRequest) -> HttpResponse:
                   {'form': SentimentForm()})
 
 
-def query_recommendations(request: HttpRequest, topn=5) -> JsonResponse:
+def query_recommendations(request: HttpRequest, topn=9) -> JsonResponse:
     # Making sure model data is fine
     assert gensim_path.is_dir(), "Gensim Directory Not Correct"
-    print('Hello there, General Kenobi!')
-    if request.method == 'POST':
-        form = SentimentForm(request.POST)
+    request_data = json.loads(request.body)
+    movies_liked = request_data[LIKE]
+    movies_liked_int = [int(i) for i in movies_liked]
+    movies_disliked = request_data[DISLIKE]
+    movies_disliked_int = [int(i) for i in movies_disliked]
 
-        if form.is_valid():
-            print(form.cleaned_data)
-            # gensim_model_str = form.cleaned_data['gensim_model']
-            gensim_model_str = EMBEDDER
-            movies_liked = form.cleaned_data['likes'].replace(' ', '').split(',')
-            movies_liked_int = [int(i) for i in movies_liked]
-            movies_disliked = form.cleaned_data['dislikes'].replace(' ', '').split(',')
-            movies_disliked_int = [int(i) for i in movies_disliked]
-            print(movies_liked)
-            print('Likes:')
-            print(df_movies.loc[movies_liked_int])
-            print('Dislikes:')
-            print(df_movies.loc[movies_disliked_int])
+    if (len(movies_liked) + len(movies_disliked)) == 0:
+        print('No Data: Random')
+        response = {MOVIE_CHOICES: random_movie_ids(9)}
+        return JsonResponse(response)
+    else:
+        print('Have Data: Calculating...')
+        gensim_model_str = EMBEDDER
+        print('Likes:')
+        print(movies_liked)
+        print(df_movies.loc[movies_liked_int])
+        print('Dislikes:')
+        print(movies_disliked)
+        print(df_movies.loc[movies_disliked_int])
 
-            gensim_model_path = gensim_path / gensim_model_str
-            if not gensim_model_path.is_file():
-                raise FileNotFoundError
+        gensim_model_path = gensim_path / gensim_model_str
+        if not gensim_model_path.is_file():
+            raise FileNotFoundError
 
-            if gensim_model_str in dict_gensim_models.keys():
-                model = dict_gensim_models[gensim_model_str]
-            else:
-                model = Word2Vec.load(str(gensim_model_path))
-                dict_gensim_models[gensim_model_str] = model
+        if gensim_model_str in dict_gensim_models.keys():
+            model = dict_gensim_models[gensim_model_str]
+        else:
+            model = Word2Vec.load(str(gensim_model_path))
+            dict_gensim_models[gensim_model_str] = model
 
-            movies_similar = model.most_similar(positive=movies_liked,
-                                                negative=movies_disliked,
-                                                topn=topn)
+        movies_similar = model.most_similar(positive=movies_liked,
+                                            negative=movies_disliked,
+                                            topn=topn)
 
-            print('Similar: ')
-            print(df_movies.loc[[int(i[0]) for i in movies_similar]])
+        print('Similar: ')
+        print(df_movies.loc[[int(i[0]) for i in movies_similar]])
 
-    return JsonResponse({'mytext': 'I have some recommendations for you!'})
+        # returns List of (movieID, similarity). We only want movieID to return for now.
+        response = {MOVIE_CHOICES: [i[0] for i in movies_similar]}
+        print(response)
+        return JsonResponse(response)
