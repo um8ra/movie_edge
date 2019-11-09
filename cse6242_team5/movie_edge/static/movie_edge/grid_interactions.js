74afe5fe50dd7c0db434b037a3d9ec67c2ebbe95
Daniel Klass <git@danielklass.com>
2019-11-08 18:33:05 -0800

    Moving more functions out of HTML.

--- cse6242_team5/movie_edge/static/movie_edge/grid_interactions.js
+++ cse6242_team5/movie_edge/static/movie_edge/grid_interactions.js
@@ -39,3 +39,54 @@ function abstractFetch(fetchPayload, fetchURL) {
             gridNine(movieChoices);
         })
 }
+
+function gridNine(movieidList) {
+    // Delete current grid and redraw with new *choices*
+    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    const grid = document.getElementById(gridID);
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    // https://stackoverflow.com/questions/2735881/adding-images-to-an-html-document-with-javascript
+    movieidList.forEach(function (movieId) {
+            const dataIndex = decoder[movieId];
+            console.log(dataIndex);
+            const divNode = document.createElement('div');
+            const imgNode = document.createElement('img');
+            const buttonLike = document.createElement('BUTTON');
+            buttonLike.innerText = 'Like';
+            buttonLike.onclick = buttonClickLike;
+            buttonLike.value = movieId;
+            const buttonDislike = document.createElement('BUTTON');
+            buttonDislike.innerText = 'Dislike';
+            buttonDislike.onclick = buttonClickDislike;
+            buttonDislike.value = movieId;
+            imgNode.src = data[dataIndex][POSTER_URL];
+            imgNode.height = POSTER_HEIGHT;
+            imgNode.width = POSTER_WIDTH;
+            grid.appendChild(divNode);
+            divNode.appendChild(imgNode);
+            divNode.appendChild(buttonLike);
+            divNode.appendChild(buttonDislike);
+            console.log(movieId);
+        }
+    )
+}
+
+function buttonClickLike(data) {
+    const movieId = data.target.value;
+    if (moviesDisliked.has(movieId)) {
+        moviesDisliked.delete(movieId);
+    }
+    moviesLiked.add(movieId);
+    console.log(moviesLiked);
+}
+
+function buttonClickDislike(data) {
+    const movieId = data.target.value;
+    if (moviesLiked.has(movieId)) {
+        moviesLiked.delete(movieId);
+    }
+    moviesDisliked.add(movieId);
+    console.log(moviesDisliked);
+}
