86e67c527ba855ea7b8c7501d1b630db09d5eaca
Daniel Klass <git@danielklass.com>
2019-11-11 20:24:04 -0800

    Adding jupyter version of payload maker.

--- cse6242_team5/movie_edge/models.py
+++ cse6242_team5/movie_edge/models.py
@@ -6,21 +6,19 @@ class Movie(models.Model):
     movie_id = models.IntegerField(null=False)  # Movie ID based on movielens data
     movie_title = models.CharField(max_length=256, null=False)
     genres = models.CharField(max_length=128, null=False)
-	
-	
+
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
-    
-	# cluster labels
+
+    # cluster labels
     L0 = models.IntegerField(null=False)
     L1 = models.IntegerField(null=False)
     L3 = models.IntegerField(null=False)
     L2 = models.IntegerField(null=False)
     L4 = models.IntegerField(null=False)
     L5 = models.IntegerField(null=False)
-	
-	
-	# x/y coordinates of clusters 
+
+    # x/y coordinates of clusters
     L0x = models.FloatField(null=False)
     L0y = models.FloatField(null=False)
     L1x = models.FloatField(null=False)
@@ -33,8 +31,7 @@ class Movie(models.Model):
     L4y = models.FloatField(null=False)
     L5x = models.FloatField(null=False)
     L5y = models.FloatField(null=False)
-	
-    
+
     poster_url = models.CharField(max_length=256, null=True)
     runtime = models.IntegerField(null=True)
     director = models.CharField(max_length=256, null=False)
@@ -48,62 +45,52 @@ class Movie(models.Model):
             models.UniqueConstraint(fields=['embedder', 'movie_id'], name='unique_movie_per_embedding'),
         ]
 
-		
-
 
 class c0(models.Model):
-    
-    cluster_id = models.IntegerField(null=False,db_index=True) 
+    cluster_id = models.IntegerField(null=False, db_index=True)
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
     genres = models.CharField(max_length=512, null=False)
     actors = models.CharField(max_length=512, null=False)
     metascore = models.IntegerField(null=True)
     imdb_rating = models.FloatField(null=True)
-    
 
-		
+
 class c1(models.Model):
-    
-    cluster_id = models.IntegerField(null=False,db_index=True) 	
+    cluster_id = models.IntegerField(null=False, db_index=True)
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
     genres = models.CharField(max_length=512, null=False)
     actors = models.CharField(max_length=512, null=False)
     metascore = models.IntegerField(null=True)
     imdb_rating = models.FloatField(null=True)
-    
 
-		
+
 class c2(models.Model):
-    
-    cluster_id = models.IntegerField(null=False,db_index=True) 
+    cluster_id = models.IntegerField(null=False, db_index=True)
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
     genres = models.CharField(max_length=512, null=False)
     actors = models.CharField(max_length=512, null=False)
     metascore = models.IntegerField(null=True)
     imdb_rating = models.FloatField(null=True)
-    
+
 
 class c3(models.Model):
-    
-    cluster_id = models.IntegerField(null=False,db_index=True) 
+    cluster_id = models.IntegerField(null=False, db_index=True)
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
     genres = models.CharField(max_length=512, null=False)
     actors = models.CharField(max_length=512, null=False)
     metascore = models.IntegerField(null=True)
     imdb_rating = models.FloatField(null=True)
-    
-		
-		
+
+
 class c4(models.Model):
-    cluster_id = models.IntegerField(null=False,db_index=True) 
+    cluster_id = models.IntegerField(null=False, db_index=True)
     x = models.FloatField(null=False)  # X value post t-SNE
     y = models.FloatField(null=False)  # Y value post t-SNE
     genres = models.CharField(max_length=512, null=False)
     actors = models.CharField(max_length=512, null=False)
     metascore = models.IntegerField(null=True)
     imdb_rating = models.FloatField(null=True)
-  	
