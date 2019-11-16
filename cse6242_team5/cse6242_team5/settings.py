efdb4c07273527006ea3567d198168e6998e5a35
rgraziano3 <rgraziano3@gatech.edu>
2019-11-16 09:43:35 -0500

    Adding hostname

--- cse6242_team5/cse6242_team5/settings.py
+++ cse6242_team5/cse6242_team5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rx4pzh7g4yomw*kg@#4hc))=p%eeyfd_%mr!shi)3&8-1c(#%%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   #TODO flip this to False
 
-ALLOWED_HOSTS = ['rockofmaine4989.pythonanywhere.com']
+ALLOWED_HOSTS = ['rockofmaine4989.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
