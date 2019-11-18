5ad7d7deac033cf1eafc465556d676ee4fdf0b50
Daniel Klass <git@danielklass.com>
2019-11-17 16:34:40 -0800

    Error free.

--- cse6242_team5/movie_edge/urls.py
+++ cse6242_team5/movie_edge/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'movie_edge'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('no_graphic/', views.index_no_graphic, name='index_no_graphic'),
     path('query_recommendations/', views.query_recommendations, name='query_recommendations'),
 ]
